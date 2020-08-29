e059165013a7689f28cad4fd5a47c7cc9800cee5
giorgiomannarini <giorgio.mannarini@gmail.com>
2020-08-29 17:53:01 +0200

    form logic added

---
+++ form.php
@@ -0,0 +1,70 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$email_error = "";
+$message_error = "";
+
+$errorMessage = '';
+
+if (!empty($_POST)) {
+
+    $email = $_POST['email_address'];
+    $message = $_POST['message'];
+
+    if (empty($email)) {
+        $email_error = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_error = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $message_error = 'Message is empty';
+    }
+
+
+    if (!empty($email_error) || !empty($message_error)) {
+        //$allErrors = urlencode(join('<br/>', $errors));
+        //$errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+        $params = !empty($email_error) ? "email_error=".$email_error : "";
+        $params .= !empty($message_error) ? (empty($params) ? "" : "&")."message_error=".$message_error  : "";
+    
+        header("Location: index.php?{$params}#contacts");
+    } else {
+        $mail = new PHPMailer();
+
+        // specify SMTP credentials
+        $mail->isSMTP();
+        $mail->SMTPDebug  = 1;
+        $mail->SMTPAuth   = TRUE;
+        $mail->SMTPSecure = "tls";
+        $mail->Port       = 587;
+        $mail->Host       = "smtp.gmail.com";
+        $mail->Username   = "giorgio.mannarini@gmail.com";
+        $mail->Password   = "YOU TRIED!!!";
+
+        $mail->setFrom($email, 'My Website');
+        $mail->Subject = 'New message from your website';
+        $mail->AddAddress("giorgio.mannarini@gmail.com", "My Website");
+
+        // Enable HTML if needed
+        $mail->isHTML(true);
+
+        $bodyParagraphs = ["Email: {$email}", "Message:", nl2br($message)];
+        $body = join('<br />', $bodyParagraphs);
+        $mail->Body = $body;
+
+        echo $body;
+        if ($mail->send()) {
+
+            header('Location: ./index.php?confirm#contacts'); // redirect to 'thank you' page
+        } else {
+            $errorMessage = urlencode('Oops, something went wrong.'); //Mailer Error: ' . $mail->ErrorInfo;
+
+            header("Location: index.php?error={$errorMessage}#contacts");
+        }
+    }
+} else {
+    header("Location: ./index.php");
+}
--- script.js
+++ script.js
@@ -1,91 +1,95 @@
 $(function () {
-        // GLOBAL VARIABLES
-        var scrolledUp = false
-        var cardsClosed = true
-        // Mobile first scrolling
-        var startingY = 0
-        var endingY = 0
-        var mobilePerc = 0
+  // GLOBAL VARIABLES
+  var scrolledUp = false
+  var cardsClosed = true
+  // Mobile first scrolling
+  var startingY = 0
+  var endingY = 0
+  var mobilePerc = 0
 
-        // FUNCTIONS
-        const animateDivTop = selector => {
-          $(selector).animate({ top: '-100%' }, 500, function () {
-            $('body').css('overflow', 'auto')
-          })
-        }
+  // FUNCTIONS
+  const animateDivTop = selector => {
+    $(selector).animate({ top: '-100%' }, 500, function () {
+      $('body').css('overflow', 'auto')
+    })
+  }
 
-        const animateDivBottom = selector => {
-          $(selector).animate({ top: '0%' }, 500)
-        }
+  const animateDivBottom = selector => {
+    $(selector).animate({ top: '0%' }, 500)
+  }
 
-        $.fn.isInViewport = function () {
-          var elementTop = $(this).offset().top
-          var elementBottom = elementTop + $(this).outerHeight()
-          var viewportTop = $(window).scrollTop()
-          var viewportBottom = viewportTop + $(window).height()
-          return elementBottom > viewportTop && elementTop < viewportBottom
-        }
+  $.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top
+    var elementBottom = elementTop + $(this).outerHeight()
+    var viewportTop = $(window).scrollTop()
+    var viewportBottom = viewportTop + $(window).height()
+    return elementBottom > viewportTop && elementTop < viewportBottom
+  }
 
-        const scrollFirstUp = () => {
-          scrolledUp = true //first div scrolled
-          animateDivTop('.page-1')
-        }
+  const scrollFirstUp = () => {
+    scrolledUp = true //first div scrolled
+    animateDivTop('.page-1')
+  }
 
-        const scrollFirstDown = () => {
-          $('body').css('overflow', 'hidden')
-          scrolledUp = false
-          cardsClosed = true
-          $('.cards').toggleClass('transition')
-          animateDivBottom('.page-1')
-        }
-        const startCardAnimation = () => {
-          cardsClosed = !cardsClosed
-          setTimeout(function () {
-            $('.cards').toggleClass('transition')
-            $('#projects').addClass('div-card-form')
-          }, 700)
-        }
+  const scrollFirstDown = () => {
+    $('body').css('overflow', 'hidden')
+    scrolledUp = false
+    cardsClosed = true
+    $('.cards').toggleClass('transition')
+    animateDivBottom('.page-1')
+  }
+  const startCardAnimation = () => {
+    cardsClosed = !cardsClosed
+    setTimeout(function () {
+      $('.cards').toggleClass('transition')
+      $('#projects').addClass('div-card-form')
+    }, 700)
+  }
 
-        // EVENTS
-        $('.nav-item').click(function(){
-          if (!scrolledUp) {
-            scrollFirstUp()
-            if($('.cards').isInViewport() && cardsClosed){
-              startCardAnimation()
-            }
-          }
-          
-        })
-        //First page animation
-        $(window).on('wheel', function (e) {
-          var scroll = -e.originalEvent.wheelDelta
-          var scrollPos = $('body').scrollTop()
+  // EVENTS
+  $('.nav-item').click(function () {
+    if (!scrolledUp) {
+      scrollFirstUp()
+      if ($('.cards').isInViewport() && cardsClosed) {
+        startCardAnimation()
+      }
+    }
+  })
+  //First page animation
+  $(window).on('wheel', function (e) {
+    var scroll = -e.originalEvent.wheelDelta
+    var scrollPos = $('body').scrollTop()
 
-          // Animation for the first div
-          if ((scroll > 0) & !scrolledUp) {
-            scrollFirstUp()
-          }
-          if ((scroll < 0) & (scrollPos == 0) && scrolledUp) {
-            scrollFirstDown()
-          }
-          if ($('.cards').isInViewport() && cardsClosed && scrolledUp) {
-            startCardAnimation()
-          }
-        })
-        $(window).on('touchstart', function (e) {
-          startingY = e.changedTouches[0].pageY
-        })
+    // Animation for the first div
+    if ((scroll > 0) & !scrolledUp) {
+      scrollFirstUp()
+    }
+    if ((scroll < 0) & (scrollPos == 0) && scrolledUp) {
+      scrollFirstDown()
+    }
+    if ($('.cards').isInViewport() && cardsClosed && scrolledUp) {
+      startCardAnimation()
+    }
+  })
+  $(window).on('touchstart', function (e) {
+    startingY = e.changedTouches[0].pageY
+  })
 
-        //mobile scrolling for the first page
-        $(window).on('touchend', function (e) {
-          endingY = e.changedTouches[0].pageY
+  //mobile scrolling for the first page
+  $(window).on('touchend', function (e) {
+    endingY = e.changedTouches[0].pageY
 
-          mobilePerc = endingY - startingY
-          if (mobilePerc < -150 && !scrolledUp) {
-            scrollFirstUp()
-          }
-          if ($('.cards').isInViewport() && cardsClosed && scrolledUp) {
-            startCardAnimation()
-          }
-        })
-      })
+    mobilePerc = endingY - startingY
+    if (mobilePerc < -150 && !scrolledUp) {
+      scrollFirstUp()
+    }
+    if ($('.cards').isInViewport() && cardsClosed && scrolledUp) {
+      startCardAnimation()
+    }
+  })
+
+ 
+  if (window.location.href.indexOf('#') > 0) {
+    scrollFirstUp()
+}
+})
